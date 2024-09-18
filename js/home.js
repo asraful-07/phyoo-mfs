eb15fc51e2a3103f303d9be0e7367e88c814acc3
asraful-07 <asraful0508@gmail.com>
2024-09-18 20:30:48 +0600

    added mony

---
+++ js/home.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-money')
+   .addEventListener('click', function(event){
+    event.preventDefault();
+   const inputMoneyAdd = document.getElementById('input-money-add').value;
+   const inputPin = document.getElementById('input-pin').value;
+     console.log(inputMoneyAdd, inputPin);
+
+    
+});
--- js/index.js
+++ js/index.js
@@ -5,5 +5,14 @@ document.getElementById('btn-login').addEventListener('click', function(event){
     console.log('step - 1 set')
     // step-3 : get the phone number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('you are logged in');
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('wrong phone number or pin.')
+    }
 })
